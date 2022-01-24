4be831d797b2d5882548a1ba2b55ec7384b4d378
Harsh Khandelwsal <007harshkhandelwal@gmail.com>
2022-01-24 13:47:42 +0530

    100 Algorithm Challenge
    In this repository I will be solving 100 algorithm daily and posting them here. Language Used will be TypeScript or JavaScript.
    - Day 1. Absolute vale sum minimization using java and typescript

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # 100AlgorithmChallenge
 In this repository I will be solving 100 algorithm daily and posting them here. Language Used will be TypeScript or JavaScript.
+
+* 1. Absolute vale sum minimization using java and typescript 
+* 2. 
+* 3. 
+* 4. 
+* 5. 
+* 6. 
+* 7. 
+* 8. 
+* 9. 
+* 10. 
+* 11. 
+* 12. 
+* 13. 
+* 14. 
+* 15. 
+* 16. 
+* 17. 
+* 18. 
+* 19. 
+* 20. 
+* 21. 
+* 22. 
+* 23. 
+* 24. 
+* 25. 
+
---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.java
@@ -0,0 +1,18 @@
+package absoluteValuesSumMinization;
+
+
+public class absoluteValuesSumMinization{
+
+    public static int absoluteValuesSum(int [] a){
+        boolean isEven = a.length % 2 == 0;
+        int len = a.length / 2;
+        int l = (int)Math.floor(len);
+        return isEven? a[len - 1] : a[l];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2,4,7};
+        int hello = absoluteValuesSum(arr);
+        System.out.println(hello);
+    }
+}
